d9614d4f2fe8ebdda8c3837ce06022ea532245dd
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-11-08 11:33:23 -0300

    Adicionando arquivo via upload

---
+++ 1.cpp
@@ -0,0 +1,32 @@
+
+	#include<iostream>
+	#include<math.h>
+        using namespace std;
+	int main ()
+
+	{
+
+        // Declarando variáveis
+
+	int a;
+	float b,c;
+
+	// Pedindo valores ao usuário
+
+	cout << endl << "Digite um valor inteiro: ";
+	cin >> a;
+
+	cout << endl << "Digite um valor real: ";
+	cin >> b;
+
+	// Operação
+
+	c = a + b;
+
+	// Resultado
+
+	cout << endl << "O resultado da soma dos números digitados é:  "<< c << endl << endl;
+
+	return 0;
+
+	}
